8a73df1ab2592ba5eb2d0bf614af1f6c8044dc39
Gilbert Lee <gilbert@bytecodelab.com>
2015-09-10 15:46:22 +0900

    implemented forum

--- js/panels/forums.js
+++ js/panels/forums.js
@@ -5,9 +5,58 @@
         return {
             restrict:"E",
             templateUrl:"views/panels/forums.html",
-            controller: function() {
+          controller: function($scope, $q) {
+            $scope.forumKey = "notice";
+            $scope.threadCount = 0;
 
-            },
+            this.refreshForum = function() {
+              this.refreshForumCount();
+              this.refreshThreads();
+            }
+
+
+            this.refreshForumCount = function() {
+              $scope.threadCount = 0;
+
+
+              var deferred = $q.defer();
+              var promise = deferred.promise;
+              promise.then(function(result){
+                $scope.threadCount = result;
+              }, function (reason) {
+                $scope.threadCount = 0;
+
+                alert(reason);
+              });
+
+              var p = Hive5.Forum.countThreads($scope.forumKey);
+              p.then(function(response) {
+                var jsonData = JSON.parse(response.raw);
+                deferred.resolve(jsonData.count);
+              });
+            };
+
+            this.refreshThreads = function() {
+              $scope.threads = [];
+
+
+              var deferred = $q.defer();
+              var promise = deferred.promise;
+              promise.then(function(result){
+                $scope.threads = result;
+              }, function (reason) {
+                $scope.threads = [];
+
+                alert(reason);
+              });
+
+              var p = Hive5.Forum.listThreads($scope.forumKey, "dec", 0, 20);
+              p.then(function(response) {
+                var jsonData = JSON.parse(response.raw);
+                deferred.resolve(jsonData.threads);
+              });
+            };
+          },
             controllerAs:"forumsPanelCtrl"
         }
     });
